c3d37a8d89db2db175bf47d652b64697c47b2625
krauscode920 <155598891+krauscode920@users.noreply.github.com>
2025-02-23 17:34:47 -0600

    tired

--- YFlow /examples/stock_prediction.py
+++ YFlow /examples/stock_prediction.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(sys.path)
+
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from typing import Tuple
